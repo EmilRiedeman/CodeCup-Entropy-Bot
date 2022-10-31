1d4ec556e32d617dd420c3a48231c721e03aba15
Saddig <riedeman.emil@gmail.com>
2022-10-31 19:13:41 +0100

    moved using namespace

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,11 @@
 #include "entropy/benchmark.hpp"
-#include "entropy/io_util.hpp"
 #include "entropy/referee.hpp"
 
 #include <cstring>
 
-using namespace entropy;
-
 int main(int argc, const char *args[]) {
+    using namespace entropy;
+
     if (argc == 1) {
         start_console_game();
     } else if (argc >= 2) {
