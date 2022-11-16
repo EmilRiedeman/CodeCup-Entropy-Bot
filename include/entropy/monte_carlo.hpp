0f737bffc5fe592b65fc429ecab0a47eb56afdb5
Saddig <riedeman.emil@gmail.com>
2022-11-16 22:41:11 +0100

    monte carlo progress

--- include/entropy/monte_carlo.hpp
+++ include/entropy/monte_carlo.hpp
@@ -13,27 +13,43 @@ class ChaosNode;
 
 class OrderNode {
 public:
-    explicit OrderNode(const Board &b) : board(b) {}
+    OrderNode() = delete;
+
+    OrderNode(
+            const Board &b,
+            const ChipPool &pool) : board(b), pool(pool) { init(); }
+
+    OrderNode(
+            ChaosNode *p,
+            const Board::ChaosMove &new_move);
 
 private:
+    void init();
+
     Board board;
+    const ChipPool pool;
+    ChaosNode *const parent{};
     std::vector<std::unique_ptr<ChaosNode>> children{};
-    const std::weak_ptr<ChaosNode> parent{};
+    uint unvisited{};
+
+    std::array<Board::OrderMove::Compact, BOARD_AREA * 2> moves{};// not safe size
+
+    friend ChaosNode;
 };
 
 class ChaosNode {
 public:
     ChaosNode() = delete;
 
-    ChaosNode(const Board &b, Colour c) : board(b), colour(c) { init(); }
+    ChaosNode(
+            const Board &b,
+            const ChipPool &pool,
+            Colour c) : board(b), pool(pool), colour(c) { init(); }
 
-    ChaosNode(const Board &b,
-              Colour c,
-              OrderNode *p,
-              const Board::OrderMove &last_move) : board(b), parent(p), colour(c) {
-        board.move_chip(last_move);
-        init();
-    };
+    ChaosNode(
+            OrderNode *p,
+            const Board::OrderMove &new_move,
+            Colour c);
 
     OrderNode *add_random_child();
 
@@ -41,11 +57,15 @@ private:
     void init();
 
     Board board;
-    std::vector<std::unique_ptr<OrderNode>> children{};
+    const ChipPool pool;
     OrderNode *const parent{};
+    std::vector<std::unique_ptr<OrderNode>> children{};
+    uint unvisited{};
+
     const Colour colour;
     std::array<uint8_t, BOARD_AREA> moves{};
-    uint unvisited{};
+
+    friend OrderNode;
 };
 
 }// namespace entropy::mcts
--- source/entropy/monte_carlo.cpp
+++ source/entropy/monte_carlo.cpp
@@ -4,14 +4,32 @@ namespace entropy::mcts {
 
 FastRand RNG{};
 
+OrderNode::OrderNode(ChaosNode *p, const Board::ChaosMove &new_move) : board(p->board), pool(p->pool), parent(p) {
+    board.place_chip(new_move);
+    init();
+}
+
+void OrderNode::init() {
+    board.for_each_possible_order_move([this](auto &&x) {
+        moves[unvisited++] = Board::OrderMove::Compact(x);
+    });
+    std::shuffle(moves.begin(), moves.begin() + unvisited, RNG);
+}
+
+ChaosNode::ChaosNode(OrderNode *p, const Board::OrderMove &new_move, Colour c) : board(p->board), pool(p->pool, c), parent(p), colour(c) {
+    board.move_chip(new_move);
+    init();
+}
+
 void ChaosNode::init() {
     board.for_each_empty_space([this](Position p) {
         moves[unvisited++] = p.p;
     });
     std::shuffle(moves.begin(), moves.begin() + unvisited, RNG);
 }
+
 OrderNode *ChaosNode::add_random_child() {
-    children.push_back(std::make_unique<OrderNode>(board, this, moves[--unvisited]));
+    children.push_back(std::make_unique<OrderNode>(this, Board::ChaosMove{moves[--unvisited], colour}));
     return children.back().get();
 }
 
