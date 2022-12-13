8cfbf3d13869fe0fede017705635f99542514052
Emil <riedeman.emil@gmail.com>
2022-12-13 19:43:41 +0100

    added pre-allocated buffer class (not thoroughly tested)

--- include/entropy/util.hpp
+++ include/entropy/util.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <memory>
 #include <random>
 
 namespace entropy {
@@ -22,8 +23,9 @@ struct FastRand {
 
 template <typename InputIterator, typename Generator>
 InputIterator random_element(InputIterator begin, typename std::remove_reference_t<Generator>::result_type n, Generator &&gen) {
-    return std::advance(begin, std::uniform_int_distribution<typename std::remove_reference_t<Generator>::result_type>{
-                                       0, n - 1}(std::forward<Generator>(gen)));
+    std::advance(begin, std::uniform_int_distribution<typename std::remove_reference_t<Generator>::result_type>{
+                                0, n - 1}(std::forward<Generator>(gen)));
+    return begin;
 }
 
 template <uint N>
@@ -75,5 +77,59 @@ template <std::size_t N, typename T>
 constexpr std::array<T, N> create_array(const T &value) {
     return create_array(value, std::make_index_sequence<N>());
 }
+
+template <typename T, std::size_t N>
+class PreallocatedBuffer {
+public:
+    typedef T value_type;
+    typedef value_type *pointer;
+
+    constexpr static inline std::size_t max_capacity = N;
+
+    struct Deleter {
+        PreallocatedBuffer &buffer;
+
+        void operator()(pointer ptr) { buffer.deallocate(ptr); }
+    };
+
+    [[nodiscard]] pointer allocate() {
+        if (gap_amount) {
+            pointer result = gaps[0];
+            gaps[0] = gaps[--gap_amount];
+            return result;
+        } else {
+            return next++;
+        }
+    }
+
+    void deallocate(pointer ptr) {
+        ptr->~value_type();
+        gaps[gap_amount++] = ptr;
+    }
+
+    Deleter get_deleter() { return {*this}; }
+
+    template <typename... Args>
+    [[nodiscard]] pointer construct(Args &&...args) {
+        return new (allocate()) T(std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    std::unique_ptr<T> make_unique(Args &&...args) {
+        return std::unique_ptr<T>(construct(std::forward<Args>(args)...), get_deleter());
+    }
+
+    template <typename... Args>
+    std::shared_ptr<T> make_shared(Args &&...args) {
+        return std::shared_ptr<T>(construct(std::forward<Args>(args)...), get_deleter());
+    }
+
+private:
+    value_type buffer[max_capacity];
+    pointer gaps[max_capacity];
+
+    pointer next = buffer;
+    std::size_t gap_amount = 0;
+};
 
 }// namespace entropy
