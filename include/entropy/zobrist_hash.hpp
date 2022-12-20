a5bad25d7638c4582aeb216ce489ae8b8e5a1013
Emil <riedeman.emil@gmail.com>
2022-12-20 23:20:35 +0100

    changed seed

--- include/entropy/zobrist_hash.hpp
+++ include/entropy/zobrist_hash.hpp
@@ -5,8 +5,8 @@
 
 namespace entropy {
 
-constexpr auto generate_zobrist_state_table(std::uint64_t SEED = 123123123123ull) {
-    MersenneTwisterEngine64 gen(SEED);
+constexpr auto generate_zobrist_state_table(std::uint64_t seed = 9876543210123456789ull) {
+    MersenneTwisterEngine64 gen(seed);
 
     std::array<std::array<std::uint64_t, BOARD_AREA>, BOARD_COLOURS> result{};
 
